d7937eae370f5b42885c8bee2c5b8181d3c1db96
IvanPartsunev <ivan.parcunev@gmail.com>
2025-12-28 15:46:02 +0200

    chore: add `drop` table command to CLI for dropping tables within a namespace
    - Introduce `drop_table_cmd` to facilitate dropping tables with confirmation prompts.
    - Leverage `typer.Option` for user input and rich feedback with emojis.
    - Add error handling via `handle_catalog_error` for better command reliability.
    - Register `drop_app` in table CLI commands for seamless integration.

--- deltacat_cli/table/__init__.py
+++ deltacat_cli/table/__init__.py
@@ -2,6 +2,8 @@ import typer
 
 from deltacat_cli.table.create import app as create_app
 from deltacat_cli.table.get import app as get_app
+from deltacat_cli.table.drop import app as drop_app
+from deltacat_cli.table.alter import app as alter_app
 
 
 app = typer.Typer()
@@ -9,4 +11,5 @@ app = typer.Typer()
 
 app.add_typer(get_app)
 app.add_typer(create_app)
+app.add_typer(drop_app)
 app.add_typer(alter_app)
